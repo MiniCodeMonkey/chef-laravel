4c580b7459418d1a7e010010fbb0b89511128279
Mathias Hansen <me@codemonkey.io>
2013-09-30 10:59:00 -0400

    Disabling default vhost

--- recipes/vhost.rb
+++ recipes/vhost.rb
@@ -5,18 +5,20 @@
 # Copyright 2013, Mathias Hansen
 #
 
+# Disable default vhost config (File.symlink? from the original apache_site definition doesn't work correctly)
+execute "a2dissite default" do
+  user "root"
+  command "/usr/sbin/a2dissite 000-default"
+  notifies :restart, resources(:service => "apache2")
+end
+
 # Define app name
 app_name = "#{node['laravel']['name']}"
 
 # Enable vhost
-web_app "0000-" + app_name do
+web_app "000-" + app_name do
   server_name node['laravel']['server_name']
   server_aliases node['laravel']['server_aliases']
   docroot node['laravel']['docroot']
   log_dir node['apache']['log_dir'] 
-end
-
-# Disable default vhost config
-apache_site "default" do
-  enable false
 end
